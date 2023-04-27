0d47306c724df55abe6cb27b8ed91255577132a2
Reed Hamilton <rhamilt@uw.edu>
2023-04-27 20:23:42 +0000

    Make start script actually use GPU when asked

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,10 +13,10 @@ if [ $1 = "cpu" ]; then
   fi
 else
   if [ $2 = "load" ]; then
-    docker run --gpus=1 --rm --net=host -v ${PWD}/model_repo_cpu:/models tritonserver \
+    docker run --gpus=1 --rm --net=host -v ${PWD}/model_repo:/models tritonserver \
       tritonserver --model-repository=/models
   else
-    docker run --gpus=1 --rm --net=host -v ${PWD}/model_repo_cpu:/models tritonserver \
+    docker run --gpus=1 --rm --net=host -v ${PWD}/model_repo:/models tritonserver \
       tritonserver --model-repository=/models --model-control-mode=explicit
   fi
 fi

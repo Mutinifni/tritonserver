238a678780626cd9fc86484e326b499ac38a2697
Reed Hamilton <rhamilt@uw.edu>
2023-05-07 17:36:42 +0000

    Update dcgm scripts to poll more frequently

--- scripts/dcgm.sh
+++ scripts/dcgm.sh
@@ -5,7 +5,7 @@
 for path in ${PWD}/perf_repo/*
 do
     f=$(basename "$path")
-    taskset -c 1,3,5,7 dcgmi dmon -e 150,155,156,203,204 > dcgm_output/$f.csv &
+    taskset -c 1,3,5,7 dcgmi dmon -d 100 -e 150,155,156,203,204 > dcgm_output/$f.csv &
     taskset -c 0,2,4,6 perf_analyzer -m $f --percentile=99
     pkill -f dcgmi;
 done

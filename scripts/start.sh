6ed5d2a645fbcb18fe2a6da4303b67b8e9b70115
Reed Hamilton <rhamilt@uw.edu>
2023-05-07 17:15:57 +0000

    Allow server to startup using different model_repos

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,18 +5,18 @@
 
 if [ $1 = "cpu" ]; then
   if [ $2 = "load" ]; then
-    docker run --gpus device=none --rm --net=host -v ${PWD}/model_repo_cpu:/models tritonserver \
+    docker run --gpus device=none --rm --net=host -v ${PWD}/$3_repo_cpu:/models tritonserver \
       tritonserver --model-repository=/models
   else
-    docker run --gpus device=none --rm --net=host -v ${PWD}/model_repo_cpu:/models tritonserver \
+    docker run --gpus device=none --rm --net=host -v ${PWD}/$3_repo_cpu:/models tritonserver \
       tritonserver --model-repository=/models --model-control-mode=explicit
   fi
 else
   if [ $2 = "load" ]; then
-    docker run --gpus=1 --rm --net=host -v ${PWD}/model_repo:/models tritonserver \
+    docker run --gpus=1 --rm --net=host -v ${PWD}/$3_repo:/models tritonserver \
       tritonserver --model-repository=/models
   else
-    docker run --gpus=1 --rm --net=host -v ${PWD}/model_repo:/models tritonserver \
+    docker run --gpus=1 --rm --net=host -v ${PWD}/$3_repo:/models tritonserver \
       tritonserver --model-repository=/models --model-control-mode=explicit
   fi
 fi

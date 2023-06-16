f82137ba3894fce7be09e9d4cab5c201c52ea280
Reed Hamilton <rhamilt@uw.edu>
2023-06-16 15:16:17 +0000

    Force docker to use all cpu cores in socket 0

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,10 +5,10 @@
 
 if [ $1 = "cpu" ]; then
   if [ $2 = "load" ]; then
-    docker run --gpus device=none --rm --net=host -v ${PWD}/$3_repo_cpu:/models tritonserver \
+    docker run --cpuset-cpus="0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46" --gpus device=none --rm --net=host -v ${PWD}/$3_repo_cpu:/models tritonserver \
       tritonserver --model-repository=/models
   else
-    docker run --gpus device=none --rm --net=host -v ${PWD}/$3_repo_cpu:/models tritonserver \
+    docker run --cpuset-cpus="0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46" --gpus device=none --rm --net=host -v ${PWD}/$3_repo_cpu:/models tritonserver \
       tritonserver --model-repository=/models --model-control-mode=explicit
   fi
 else
